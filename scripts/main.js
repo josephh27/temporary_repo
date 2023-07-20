54b0238077843ea273858a78d052b172cb1b68b3
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-07-21 01:13:09 +0800

    third commit.

--- scripts/main.js
+++ scripts/main.js
@@ -72,7 +72,7 @@ $(document).ready(function () {
         autoplayHoverPause: true,
         dots: false,
         responsive: {
-            0: {items: 2},
+            0: {items: 1},
             600: {items: 2},
             1000: {items: 3}
         },
